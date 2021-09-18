6969f79e77fa8fb339d7da629266e4c6efc56796
yun8boo <yuma051521@gmail.com>
2021-09-19 02:40:11 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 渡辺悠真 on 2021/09/19.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
